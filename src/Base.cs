7788bb446d54bfab4db7e38e95f41a276973a456
Blog <57299745+shinyish@users.noreply.github.com>
2022-01-15 02:16:10 -0800

    Create Base.cs

---
+++ src/Base.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.CoreClasses
+{
+    // Core class where every instance inherits from.
+
+    class Base
+    {
+        // manifest / properties
+        string Name = "Base";
+
+        void Destroy()
+        {
+            
+        }
+    }
+}
