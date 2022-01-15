19506e3021794f758f57c2db1401777fe95bd27e
Blog <57299745+shinyish@users.noreply.github.com>
2022-01-15 15:23:28 -0800

    Update Text.cs

--- src/Text.cs
+++ src/Text.cs
@@ -12,10 +12,20 @@ namespace src
 
         public string Name = "Text";
         public string TextContent = "Lorem Ipsum";
+        public ConsoleColor TextColor = ConsoleColor.White;
 
         public void Display()
         {
+            Console.ForegroundColor = TextColor;
             Console.WriteLine(TextContent);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void SetColor(string color)
+        {
+            string lowered = color.ToLower();
+
+            TextColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), lowered);
         }
 
         public void Destroy()
